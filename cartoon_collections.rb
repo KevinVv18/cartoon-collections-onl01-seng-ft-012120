8b520ce348411553647e2c678471f53a66b563ef
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-28 19:31:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,11 +16,3 @@ def summon_captain_plane(array)
   nu_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
