5efd4c7d61a8dcdbebc0e84718a84bfb525245af
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-28 19:30:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarve(array)
+  i = 0
+  while i < array.lenght 
+    puts "#{i + 1}. #{array[1]}"
+    i += 1
+  end
 end
 
 def summon_captain_planet# code an argument here
