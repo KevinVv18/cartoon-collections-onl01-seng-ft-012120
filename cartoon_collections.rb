bf737a8ccd2caae9cd662166211afa96722f67dd
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-28 19:32:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,25 @@
-def roll_call_dwarve(array)
-  i = 0
-  while i < array.lenght 
-    puts "#{i + 1}. #{array[1]}"
-    i += 1
-  end
+def roll_call_dwarves(array)
+  index = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_plane(array)
-  nu_array = []
-  i = 0 
-  while i < array.length 
-    nu_array << array[i].capitalize + "!"
-    i += 1 
-  end 
-  nu_array
+def summon_captain_planet(array)
+  planeteer_calls = []
+  array.collect do |calls|
+    planeteer_calls << "#{calls.capitalize}!"
+  end
+  planeteer_calls
+end
+
+def long_planeteer_calls(array)
+  array.any? do |word|
+    word.length > 4
+end
+end
+
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "gouda" || cheese == "camembert"
+  end
 end
 
